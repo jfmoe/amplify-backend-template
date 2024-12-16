25c95092ef4b70535d8e06b88bfaab1e44c7e882
mingshang.tao <mingshang.tao@tuya.com>
2024-12-16 11:03:10 +0800

    feat: prefix

--- amplify/config/index.ts
+++ amplify/config/index.ts
@@ -1,4 +1,4 @@
 export type TTLConfig = Array<{ modalName: string; tableName: string; timeToLive: number }>;
 export const TTL_CONFIG = JSON.parse(process.env.TTL_CONFIG ?? '[]') as TTLConfig;
 
-export const prefix = 'body-fat-scale';
+export const prefix = 'common';
