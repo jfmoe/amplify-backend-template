275e14c6d484e45e8243c4e649c31c036529e1a9
mingshang.tao <mingshang.tao@tuya.com>
2024-12-16 16:39:08 +0800

    feat: bakcend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -28,7 +28,7 @@ const { amplifyDynamoDbTables } = backend.data.resources.cfnResources;
 // 为所有 table 开启时间点恢复功能和删除保护
 for (const table of Object.values(amplifyDynamoDbTables)) {
   table.pointInTimeRecoveryEnabled = true;
-  table.deletionProtectionEnabled = true;
+  // table.deletionProtectionEnabled = true;
 }
 
 // 根据需要为 table 开启 TTL 功能（如果当前时间超过设定的时间戳，则该条数据过期）
